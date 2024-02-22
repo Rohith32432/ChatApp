a26b86615f7f0e6d50d08427719ac4dadd994a28
Rohith_k <2100032432cse@gmail.com>
2024-02-22 23:58:41 +0530

    ntg

--- chatapp/src/components/Home.js
+++ chatapp/src/components/Home.js
@@ -1,38 +1,39 @@
-import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text, } from "@chakra-ui/react";
-
+import { Box, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
 import Login from "../Auth/Login";
 import Signup from "../Auth/Signup";
 
 function Home() {
-
     return (
         <Box
-
-            display={'flex'}
-            justifyContent={'center'}
-            height={'100vh'}
-            width={'100%'}
-            alignItems={'center'}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100vh"
+            width="100%"
         >
-            <Box display={'flex'}
-                justifyContent={'center'}
-                alignItems={'center'}
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                width="50%"
+                padding="20px"
             >
                 <Box
-                    d="flex"
+                    display="flex"
                     justifyContent="center"
                     p={3}
                     bg="white"
-                    w="100%"
-                    m="40px 0 15px 0"
+                    width="100%"
+                    mb="40px"
                     borderRadius="lg"
                     borderWidth="1px"
                 >
-                    <Text fontSize="4xl" fontFamily={'revert-layer'}>
+                    <Text fontSize="4xl" fontFamily="revert-layer">
                         Talk-A-Tive
                     </Text>
                 </Box>
-                <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+                <Box bg="white" width="100%" p={4} borderRadius="lg" borderWidth="1px">
                     <Tabs isFitted variant="soft-rounded">
                         <TabList mb="1em">
                             <Tab>Login</Tab>
@@ -49,7 +50,6 @@ function Home() {
                     </Tabs>
                 </Box>
             </Box>
-
         </Box>
     );
 }
